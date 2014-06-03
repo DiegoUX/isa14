7dc9d7da2e0e0630b2572543dca877705394c0f5
Diego Herrando <diego@theuxdepartment.com>
2014-06-03 14:53:40 -0300

    grillas para ie8

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
         <link href="<?php echo get_template_directory_uri(); ?>/foundation.min.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri(); ?>/ie8-foundation4-grid.css" rel="stylesheet">
 		<link href="<?php echo get_template_directory_uri(); ?>/assets/stylesheets/screen.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
